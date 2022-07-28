4d3f49936863859b053ebeea2d4536836a9d8960
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-07-28 07:44:12 -0500

    add comments to packages needed section

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // TODO: Include packages needed for this application
-
+// declaring reference to element using const for constant reference
+// using node.js require statement to access fs, inquirer, generatePage to access the modules functions by the const assignment
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [];
 
